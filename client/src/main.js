c1e5ac46017af10758d95d84932e49dad877ec86
xuhaorong <156456286@qq.com>
2021-01-07 10:31:25 +0800

    前端代码配置文件修改。

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router' // 导入路由
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://120.78.134.117:7001';
 Vue.prototype.$http = axios;
 
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+
+console.log('process.env',process.env)
 Vue.config.productionTip = false
 new Vue({
   router,
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-    publicPath:'/',
+    publicPath: './',
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://www.xuhaorong.cn:7001',
-                ws: true, //允许 webservice 服务
-                secure : false,
-                changeOrgin: true, //开启虚拟服务器去请求代理服务器 http://localhost:7001
-                pathRewrite: {  //路径重写
-                    "^/api": ""
-                }
-            }
+                target: 'http://www.xuhaorong.cn',
+                ws: true,
+                changeOrigin: true,
+                // pathRewrite: { //路径重写
+                //     "^/api": "/api"
+                // }
+            },
         }
     }
 }
