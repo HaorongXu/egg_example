67b9b0ba29278fd4fc273a0ca064460d5e1f2b7a
xuhaorong <156456286@qq.com>
2021-01-07 11:03:33 +0800

    配置文件修改。

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,11 +3,11 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://www.xuhaorong.cn',
+                target: 'http://www.xuhaorong.cn',  // Request URL: http://localhost:8080/api/article/lists
                 ws: true,
                 changeOrigin: true,
                 // pathRewrite: { //路径重写
-                //     "^/api": "/api"
+                //     "^/api": ""
                 // }
             },
         }
